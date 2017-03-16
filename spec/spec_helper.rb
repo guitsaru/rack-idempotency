5a6abbdc8fa71451aac8e962afbb0a899b13d83f
Matt Pruitt <guitsaru@gmail.com>
2017-03-15 20:12:31 -0700

    Fix travis build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "rack/idempotency"
 
 require "rack/mock"
+require "securerandom"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
