feba1671c3709a768162ee15b07e7cd7169c27a0
Matt Pruitt <guitsaru@gmail.com>
2017-03-15 17:35:27 -0700

    Initial Commit

---
+++ lib/rack/idempotent.rb
@@ -0,0 +1,7 @@
+require "rack/idempotent/version"
+
+module Rack
+  module Idempotent
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/idempotent/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Idempotent
+    VERSION = "0.1.0"
+  end
+end
